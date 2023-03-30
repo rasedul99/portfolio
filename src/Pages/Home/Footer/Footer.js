781d86662f0f4d0c5a6b3e4b8aeadc3ddab1a492
MD Rasedul Islam <rasedul2199@gmail.com>
2023-03-31 04:04:01 +0600

    portfolio

---
+++ src/Pages/Home/Footer/Footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+// import MessengerCustomerChat from 'react-messenger-customer-chat';
+
+const Footer = () => {
+  return (
+    <footer className="footer footer-center font-bold text-bold p-4 bg-white  text-blue-800">
+      {/* <MessengerCustomerChat
+                pageId="103460092385786"
+                appId="724354469165711"
+            />, */}
+      <div>
+        <p>Copyright © 2023 - All right reserved by MD Rasedul Islam</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
